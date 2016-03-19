b1d8716701ef55204859915f41000609c27c291b
Cagatay Gurturk <cagatay@itusozluk.com>
2016-03-20 00:42:01 +0100

    Alternatig Characters solution

---
+++ src/AlternatingCharacters.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/**
+ * @link https://www.hackerrank.com/challenges/alternating-characters
+ */
+public class AlternatingCharacters {
+
+    public static int minDeletionsRequired(String s) {
+        int minDeletion = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            String curChar = s.substring(i, i + 1);
+            String nextChar = s.substring(i + 1, i + 2);
+            if (curChar.equals(nextChar)) {
+                minDeletion++;
+            }
+        }
+        return minDeletion;
+    }
+
+    private static void process(int numberOfStrings, String[] inputStrs) {
+        int repeations = 0;
+        while (repeations < numberOfStrings) {
+            System.out.println(minDeletionsRequired(inputStrs[repeations++]));
+        }
+    }
+
+    private static String[] acceptInput(Scanner scanner, int numberOfStrings) {
+        String[] inputStrs = new String[numberOfStrings];
+        int repeations = 0;
+        while (repeations < numberOfStrings) {
+            inputStrs[repeations++] = scanner.next();
+        }
+        return inputStrs;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfStrings = scanner.nextInt();
+
+        String[] inputStrs = acceptInput(scanner, numberOfStrings);
+
+        process(numberOfStrings, inputStrs);
+
+        scanner.close();
+    }
+}
+
